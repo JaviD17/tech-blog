c25d379ed76b58868a96b5abfb8698c1c561742d
Javier Dominguez II <Javi7057@gmail.com>
2021-08-23 15:37:15 -0500

    testing works

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,11 +1,25 @@
-const { format_url } = require("../utils/helpers");
+const { format_url, format_plural, format_date } = require("../utils/helpers");
 
 test("format_url() returns a simplified url string", () => {
-    const url1 = format_url('https://test.com/page/1');
-    const url2 = format_url('https://howaboutthis.com/abcdefghijk/');
-    const url3 = format_url('https://coolthings.com?q=hi');
+  const url1 = format_url("https://test.com/page/1");
+  const url2 = format_url("https://howaboutthis.com/abcdefghijk/");
+  const url3 = format_url("https://coolthings.com?q=hi");
 
-    expect(url1).toBe("test.com");
-    expect(url2).toBe("howaboutthis.com");
-    expect(url3).toBe("coolthings.com");
+  expect(url1).toBe("test.com");
+  expect(url2).toBe("howaboutthis.com");
+  expect(url3).toBe("coolthings.com");
+});
+
+test("format_plural() returns a pluralized word", () => {
+  const word1 = format_plural("panda", 1);
+  const word2 = format_plural("dog", 2);
+
+  expect(word1).toBe("panda");
+  expect(word2).toBe("dogs");
+});
+
+test("format_date() returns a date string", () => {
+  const date = new Date("2021-02-20 14:17:05");
+
+  expect(format_date(date)).toBe("2/20/2021");
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -7,4 +7,16 @@ module.exports = {
       .split("/")[0]
       .split("?")[0];
   },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
 };
