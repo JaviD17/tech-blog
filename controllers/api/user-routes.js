b2b35b2bd9916e48efffd8e70b765b068f88b03f
Javier Dominguez II <Javi7057@gmail.com>
2021-08-24 12:23:42 -0500

    User model added and controller/api/ folder updated

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,7 +2,26 @@ const router = require('express').Router();
 // add models and require them
 
 // get all users
+router.get("/", (req, res) => {
+    // User.findAll({})
+})
 // get one user
+router.get("/:id", (req, res) => {
+    // User.findOne({})
+})
 // post user
+router.post("/", (req, res) => {
+    // expects {username: "", email: "", password: ""}
+    // User.create({})
+})
+// post user login
+router.post("/login", (req, res) => {
+    // expects {username: "", email: "", password: ""}
+    // User.findOne({})
+})
 // put user
+router.put("/:id", (req, res) => {
+    // expects {username: "", email: "", password: ""}
+    // User.update({})
+})
 // delete user
