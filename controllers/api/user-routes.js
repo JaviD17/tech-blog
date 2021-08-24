2c1f1cf11fa12745919bafca57eed359496f52ca
Javier Dominguez II <Javi7057@gmail.com>
2021-08-24 13:48:50 -0500

    updated user-routes for find all users and destroy users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,27 +1,54 @@
-const router = require('express').Router();
+const User = require("../../models/User");
+
+const router = require("express").Router();
 // add models and require them
 
 // get all users
 router.get("/", (req, res) => {
-    // User.findAll({})
-})
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 // get one user
 router.get("/:id", (req, res) => {
-    // User.findOne({})
-})
+  // User.findOne({})
+});
 // post user
 router.post("/", (req, res) => {
-    // expects {username: "", password: ""}
-    // User.create({})
-})
+  // expects {username: "", password: ""}
+  // User.create({})
+});
 // post user login
 router.post("/login", (req, res) => {
-    // expects {username: "", password: ""}
-    // User.findOne({})
-})
+  // expects {username: "", password: ""}
+  // User.findOne({})
+});
 // put user
 router.put("/:id", (req, res) => {
-    // expects {username: "", password: ""}
-    // User.update({})
-})
+  // expects {username: "", password: ""}
+  // User.update({})
+});
 // delete user
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with that id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
